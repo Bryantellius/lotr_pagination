acc0af520d7f293e7c91caa8828e494d90ad8023
Ben <brbryant2639@gmail.com>
2021-11-12 15:31:15 -0600

    Updated title and build file

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import DOM from "./utils/dom";
 DOM.fetchCharacters();
 
 DOM.character.addEventListener("keyup", (e) => DOM.toggleSearchBtn(e));
-DOM.prevBtn.addEventListener("click", () => DOM.updateCharacters(-10));
+DOM.prevBtn.addEventListener("click", () => DOM.updatePagination(-10));
 DOM.nextBtn.addEventListener("click", () => DOM.updatePagination(10));
 DOM.form.addEventListener("submit", (e) => DOM.searchCharacter(e));
